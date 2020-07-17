42dfb0913972560938e9fb96f91e7af07a61f862
timbuntu <tim.christen@gmx.de>
2020-07-17 12:08:30 +0200

    Tests: Added tests for widget child management

--- tests/test_widget.cpp
+++ tests/test_widget.cpp
@@ -2,8 +2,16 @@
 
 using namespace SdlUi;
 
+void addConstructorTests(void);
+void addChildTests(void);
+
 int main(int argc, const char** argv) {
-    
+    addConstructorTests();   
+    addChildTests();
+    runTests();
+}
+
+void addConstructorTests(void) {
     TEST_ADD("Constructor1", [](void) {
         Widget widget(nullptr);
         
@@ -39,11 +47,11 @@ int main(int argc, const char** argv) {
         Widget widgetDefault(nullptr, Vector(200, 200), Vector(400, 400));
         
         assert(widgetDefault.getPos()         == Vector(200,200));
-        assert(widgetDefault.getDim()         == Vector(400,400)  );
-        assert(widgetDefault.getParent()      == nullptr      );
-        assert(widgetDefault.freesChildren()  == false        );
-        assert(widgetDefault.isValid()        == true         );
-        assert(widgetDefault.getBorderWidth() == 0            );
+        assert(widgetDefault.getDim()         == Vector(400,400));
+        assert(widgetDefault.getParent()      == nullptr        );
+        assert(widgetDefault.freesChildren()  == false          );
+        assert(widgetDefault.isValid()        == true           );
+        assert(widgetDefault.getBorderWidth() == 0              );
 
         Widget widgetCustom(nullptr, Vector(200, 200), Vector(640, 480), 4, true);
         
@@ -54,6 +62,31 @@ int main(int argc, const char** argv) {
         assert(widgetCustom.isValid()        == true           );
         assert(widgetCustom.getBorderWidth() == 4              );
     });
+}
 
-    runTests();
+static void testParentChild(Widget* parent, Widget* child) {
+    assert(child->getParent() == parent);
+    assert(parent->hasChild(child));
+    assert(child->getPos() >= parent->getPos());
+    assert(child->getPos() + child->getDim() <= parent->getDim());
+
+    parent->delChild(child);
+    assert(parent->hasChild(child) == false);
+    assert(child->getParent() == nullptr);
+}
+void addChildTests(void) {
+    TEST_ADD("Childs via parent pointer", [](void) {
+        Widget parent(nullptr, Vector(0,0), Vector(640,480));
+        Widget child(&parent, Vector(0,0), Vector(1000,1000));
+        
+        testParentChild(&parent, &child);
+    });
+
+    TEST_ADD("Childs via addChild", [](void) {
+        Widget parent(nullptr, Vector(0,0), Vector(640,480));
+        Widget child(nullptr, Vector(100,100), Vector(1000,1000));
+        parent.addChild(&child);
+        
+        testParentChild(&parent, &child);
+    });
 }

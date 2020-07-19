4185fbd9028b22f4785a52b45affd6eb78d7a281
timbuntu <tim.christen@gmx.de>
2020-07-19 11:40:15 +0200

    Doc: Added docstrings to Vector class

--- ui/Vector.cpp
+++ ui/Vector.cpp
@@ -2,22 +2,54 @@
 
 using namespace SdlUi;
 
+/**
+ * Two Vectors are euqal, if their x and y values both are equal.
+ *
+ * @param other The Vector to compare to.
+ * @return Whether this Vector is equal to the given one
+ */
 bool Vector::operator==(const Vector& other) const {
     return x == other.x && y == other.y;
 }
 
+/**
+ * One Vector is smaller than another if their absolute, or length, is smaller.
+ *
+ * @param other The Vector to compare to.
+ * @return Whether this Vector is smaller than the given one
+ */
 bool Vector::operator<(const Vector& other) const {
     return abs(*this) < abs(other);
 }
 
+/**
+ * Two Vectors are euqal, if their x and y values both are equal.
+ * One Vector is smaller than another if their absolute, or length, is smaller.
+ *
+ * @param other The Vector to compare to.
+ * @return Whether this Vector is smaller than, or equal to, the given one
+ */
 bool Vector::operator<=(const Vector& other) const {
     return abs(*this) <= abs(other);
 }
 
+/**
+ * One Vector is bigger than another if their absolute, or length, is bigger.
+ *
+ * @param other The Vector to compare to.
+ * @return Whether this Vector is bigger than the given one
+ */
 bool Vector::operator>(const Vector& other) const {
     return abs(*this) > abs(other);
 }
 
+/**
+ * Two Vectors are euqal, if their x and y values both are equal.
+ * One Vector is bigger than another if their absolute, or length, is bigger.
+ *
+ * @param other The Vector to compare to.
+ * @return Whether this Vector is bigger than, or equal to, the given one
+ */
 bool Vector::operator>=(const Vector& other) const {
     return abs(*this) >= abs(other);
 }
@@ -34,14 +66,32 @@ Vector& Vector::operator-=(const Vector& other){
     return *this;
 }
 
+/**
+ * Adding two Vectors adds their individual components.
+ *
+ * @param other The Vector to add to this one
+ * @return The sum of this and the given Vector
+ */
 Vector Vector::operator+(const Vector& other) const {
     return Vector(x+other.x, y+other.y);
 }
 
+/**
+ * Subtracting two Vectors subtracts their individual elements.
+ *
+ * @param other The Vector to subtract from this one
+ * @return The difference of this and the given Vector
+ */
 Vector Vector::operator-(const Vector& other) const {
     return Vector(x-other.x, y-other.y);
 }
 
+/**
+ * Multiplying a Vector with a scalar multiplies its individual components with the scalar.
+ *
+ * @param other The scalar to multiply with
+ * @return The the Vector multiplied by the given scalar
+ */
 Vector Vector::operator*(const int scalar) const {
     return Vector(scalar*x, scalar*y);
 }
@@ -50,6 +100,12 @@ Vector Vector::operator* (const float scalar) const {
     return Vector(round(scalar*x), round(scalar*y));
 }
 
+/**
+ * Multiplying two Vectors of the same dimension returns a scalar which can be interpreted to decern their orientation to each other.
+ *
+ * @param other The Vector to multiply with
+ * @return The scalar product of the two Vectors
+ */
 float Vector::operator*(const Vector& other) const {
     return x*other.x + y*other.y;
 }
